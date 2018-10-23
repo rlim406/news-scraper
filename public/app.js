5402bb2696d2aa31a6e473b55b890e092e240d37
r.lim406 <r.lim406@hotmail.com>
2018-10-22 21:58:21 -0400

    still working on delete function

--- public/app.js
+++ public/app.js
@@ -11,11 +11,12 @@ $.getJSON("/notes", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#savedNotes").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].body + "</p>");
-    $("savedNotes").append('<input type="button" value="test" />');
-  }
-});
 
+    $("#savedNotes").prepend("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].body + "</span><span class=delete>Delete </span></p>");
+
+  }
+
+});
 
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function () {
@@ -78,4 +79,25 @@ $(document).on("click", "#savenote", function () {
   // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
+});
+
+$(document).on("click", ".delete", function () {
+  // Save the p tag that encloses the button
+  var thisId = $(this).attr("data-id");
+  // Make an AJAX GET request to delete the specific note
+  // this uses the data-id of the p-tag, which is linked to the specific note
+  $.ajax({
+    type: "GET",
+    url: "/delete/" + thisId,
+
+    // On successful call
+    success: function (response) {
+      // Remove the p-tag from the DOM
+      thisID.remove();
+      // Clear the note and title inputs
+      $("#note").val("");
+      $("#title").val("");
+      // Make sure the #action-button is submit (in case it's update)
+    }
+  });
 });
