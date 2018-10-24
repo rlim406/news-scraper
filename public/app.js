3a04cae45fccf082896a68801ffc67bbd66101c0
r.lim406 <r.lim406@hotmail.com>
2018-10-24 19:01:02 -0400

    changing html to handlebars

--- public/app.js
+++ public/app.js
@@ -1,105 +1,283 @@
+function showModal() {
 
-$.getJSON("/articles", function (data) {
-  for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-    $("#articles").append("<button data-id='" + data._id + "' id='savearticle'>Save Article</button>");
-  }
-});
+  console.log("working");
 
-$.getJSON("/notes", function (data) {
+  $("#modal_" + $(this).attr("data-id")).show();
 
-  for (var i = 0; i < data.length; i++) {
 
-    $("#savedNotes").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].body + "</p>");
-    $("#savedNotes").append("<button data-id='" + data._id + "' id='deleteNote'>deleteNote</button>");
-  }
-});
 
-$.getJSON("/articles/:id", function (data) {
+  function closeModal() {
 
-  for (var i = 0; i < data.length; i++) {
+    $("#modal_" + $(this).attr("data-id")).hide();
 
-    $("#savedArticle").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});
+  };
 
-$(document).on("click", "p", function () {
-  $("#notes").empty();
-  var thisId = $(this).attr("data-id");
 
-  $.ajax({
-    method: "GET",
-    url: "/articles/" + thisId
-  })
-    .then(function (data) {
-      console.log(data);
 
-      $("#notes").append("<h2>" + data.title + "</h2>");
+  $(document).on("click", ".modalClose", closeModal);
 
-      $("#notes").append("<input id='titleinput' name='title' >");
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+};
 
-      if (data.note) {
-        $("#titleinput").val(data.note.title);
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
+$(document).on("click", ".comment-button", showModal);
 
-$(document).on("click", "#savearticle", function () {
+
+$(document).on("click", "#savecomment", function () {
+
+  // Grab the id associated with the article from the submit button
+
+  console.log("click firing");
 
   var thisId = $(this).attr("data-id");
 
+  console.log("pre-comment-body");
+
+  var commentBody = $("#bodyinput-" + $(this).attr("data-id")).val();
+
+  console.log(commentBody);
+
+
+
+  // Run a POST request to change the note, using what's entered in the inputs
 
   $.ajax({
-    method: "GET",
-    url: "/articles/:id" + thisId,
-  })
 
-    .then(function (data) {
-      data.title,
-        data.link
-    });
-
-});
-
-$(document).on("click", "#savenote", function () {
-
-  var thisId = $(this).attr("data-id");
-
-
-  $.ajax({
     method: "POST",
+
     url: "/articles/" + thisId,
+
     data: {
-      title: $("#titleinput").val(),
-      body: $("#bodyinput").val()
+
+      body: commentBody
+
     }
+
   })
 
-    .then(function (data) {
+    // With that done
+
+    .done(function (data) {
+
+      // Log the response
 
       console.log(data);
 
-      $("#notes").empty();
+      // Empty the notes section
+
+      location.reload();
+
     });
 
 
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
+
 });
 
-$(document).on("click", "#deleteNote", function () {
 
-  var thisId = $(this).attr("data-id");
+
+$(document).on("click", "#deletecomment", function () {
+
+  console.log("working");
+
+  var id = $(this).attr("data-comment");
 
   $.ajax({
-    type: "GET",
-    url: "/delete/" + thisId,
 
-    success: function (response) {
-      thisId.remove();
+    method: "POST",
+
+    url: "/articles/delete/" + id,
+
+    data: {
+
     }
-  });
+
+  })
+
+
+
+    .done(function (data) {
+
+
+
+      console.log(data);
+
+
+
+      location.reload();
+
+    });
+
 });
+
+
+
+$(document).on("click", ".save-button", function () {
+
+  var id = $(this).attr("data-id");
+
+  $.ajax({
+
+    method: "POST",
+
+    url: "/articles/save/" + id,
+
+    data: {
+
+    }
+
+  })
+
+
+
+    .done(function (data) {
+
+
+
+      console.log(data);
+
+
+
+      location.reload();
+
+    });
+
+});
+
+
+
+$(document).on("click", ".unsave-button", function () {
+
+  var id = $(this).attr("data-id");
+
+  $.ajax({
+
+    method: "POST",
+
+    url: "/articles/unsave/" + id,
+
+    data: {
+
+    }
+
+  })
+
+
+
+    .done(function (data) {
+
+
+
+      console.log(data);
+
+
+
+      location.reload();
+
+    });
+
+});
+
+
+
+// $.getJSON("/articles", function (data) {
+//   for (var i = 0; i < data.length; i++) {
+//     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+//     $("#articles").append("<button data-id='" + data._id + "' id='savearticle'>Save Article</button>");
+//   }
+// });
+
+// $.getJSON("/notes", function (data) {
+
+//   for (var i = 0; i < data.length; i++) {
+
+//     $("#savedNotes").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].body + "</p>");
+//     $("#savedNotes").append("<button data-id='" + data._id + "' id='deleteNote'>deleteNote</button>");
+//   }
+// });
+
+// $.getJSON("/articles/:id", function (data) {
+
+//   for (var i = 0; i < data.length; i++) {
+
+//     $("#savedArticle").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+//   }
+// });
+
+// $(document).on("click", "p", function () {
+//   $("#notes").empty();
+//   var thisId = $(this).attr("data-id");
+
+//   $.ajax({
+//     method: "GET",
+//     url: "/articles/" + thisId
+//   })
+//     .then(function (data) {
+//       console.log(data);
+
+//       $("#notes").append("<h2>" + data.title + "</h2>");
+
+//       $("#notes").append("<input id='titleinput' name='title' >");
+//       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+//       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+//       if (data.note) {
+//         $("#titleinput").val(data.note.title);
+//         $("#bodyinput").val(data.note.body);
+//       }
+//     });
+// });
+
+// $(document).on("click", "#savearticle", function () {
+
+//   var thisId = $(this).attr("data-id");
+
+
+//   $.ajax({
+//     method: "GET",
+//     url: "/articles/:id" + thisId,
+//   })
+
+//     .then(function (data) {
+//       data.title,
+//         data.link
+//     });
+
+// });
+
+// $(document).on("click", "#savenote", function () {
+
+//   var thisId = $(this).attr("data-id");
+
+
+//   $.ajax({
+//     method: "POST",
+//     url: "/articles/" + thisId,
+//     data: {
+//       title: $("#titleinput").val(),
+//       body: $("#bodyinput").val()
+//     }
+//   })
+
+//     .then(function (data) {
+
+//       console.log(data);
+
+//       $("#notes").empty();
+//     });
+
+
+//   $("#titleinput").val("");
+//   $("#bodyinput").val("");
+// });
+
+// $(document).on("click", "#deleteNote", function () {
+
+//   var thisId = $(this).attr("data-id");
+
+//   $.ajax({
+//     type: "GET",
+//     url: "/delete/" + thisId,
+
+//     success: function (response) {
+//       thisId.remove();
+//     }
+//   });
+// });
--- server.js
+++ server.js
@@ -50,90 +50,137 @@ app.get("/scrape", function (req, res) {
   });
 });
 
-// Route for getting all Articles from the db
-app.get("/articles", function (req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function (dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+app.get("/", function (req, res) {
+
+  // TODO: Finish the route so it grabs all of the articles
+
+  db.Article.find({}).populate("comments").then(function (data) {
+
+    res.render("index", { articles: data });
+
+  }).catch(function (err) {
+
+    res.json(err);
+
+  });
+
 });
 
 
-app.get("/notes", function (req, res) {
-  // Find all Notes
-  db.Note.find({})
-    .then(function (dbNote) {
-      // If all Notes are successfully found, send them back to the client
-      res.json(dbNote);
-    })
-    .catch(function (err) {
-      // If an error occurs, send the error back to the client
-      res.json(err);
-    });
-});
 
 // Route for grabbing a specific Article by id, populate it with it's note
+
 app.get("/articles/:id", function (req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("notes")
-    .then(function (dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+
+  // TODO
+
+  // ====
+
+  // Finish the route so it finds one article using the req.params.id,
+
+  // and run the populate method with "note",
+
+  // then responds with the article with the note included
+
+  db.Article.findById(req.params.id).populate("comments").then(function (data) {
+
+    res.json(data);
+
+  }).catch(function (err) {
+
+    res.json(err);
+
+  });
+
+
+
 });
+
+
 
 // Route for saving/updating an Article's associated Note
+
 app.post("/articles/:id", function (req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
-    .then(function (dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function (dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      res.json(err);
+
+  // TODO
+
+  // ====
+
+  // save the new note that gets posted to the Notes collection
+
+  // then find an article from the req.params.id
+
+  // and update it's "note" property with the _id of the new note
+
+  db.Comment.create(req.body).then(function (dbComment) {
+
+    return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { comments: dbComment } }).then(function (dbRes) {
+
+      res.redirect("/");
+
     });
+
+  })
+
+
+
 });
 
-app.get("/delete/:id", function (req, res) {
 
-  db.Notes.remove(
-    {
-      _id: mongojs.ObjectID(req.params.id)
-    },
-    function (error, removed) {
 
-      if (error) {
-        console.log(error);
-        res.send(error);
-      }
-      else {
+app.post("/articles/delete/:id", function (req, res) {
 
-        console.log(removed);
-        res.send(removed);
-      }
-    }
-  );
+  db.Comment.remove({ _id: req.params.id }).then(function (dbRemove) {
+
+    res.json(dbRemove);
+
+  });
+
 });
+
+
+
+app.post("/articles/save/:id", function (req, res) {
+
+  db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: true }).then(function (dbRes) {
+
+    res.redirect("/");
+
+  })
+
+})
+
+
+
+app.post("/articles/unsave/:id", function (req, res) {
+
+  db.Article.findOneAndUpdate({ _id: req.params.id }, { saved: false }).then(function (dbRes) {
+
+    res.redirect("/");
+
+  })
+
+})
+
+
+
+app.get("/savedarticles", function (req, res) {
+
+  // TODO: Finish the route so it grabs all of the articles
+
+  db.Article.find({ saved: true }).populate("comments").then(function (data) {
+
+    res.render("saved", { articles: data });
+
+  }).catch(function (err) {
+
+    res.json(err);
+
+  });
+
+});
+
+
 // Start the server
 app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
